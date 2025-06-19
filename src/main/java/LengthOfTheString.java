bb0bdfd06ab5937af623a58533241895c934a04c
moinuddin06218 <moinuddin06218@gmail.com>
2025-06-19 00:44:48 -0400

    Commit7

---
+++ src/main/java/LengthOfTheString.java
@@ -0,0 +1,12 @@
+public class LengthOfTheString {
+
+    public static void main(String[] args) {
+
+        String str1 = "New Jersey";
+        String str2 = "Jersey";
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        String str3 = str1+" "+str2;
+        System.out.println(str3.length());
+    }
+}
