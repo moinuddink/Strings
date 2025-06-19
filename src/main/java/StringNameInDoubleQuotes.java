8d5649701d5a670c5238511b14d108a3f947643a
moinuddin06218 <moinuddin06218@gmail.com>
2025-06-19 00:35:52 -0400

    Commit6

---
+++ src/main/java/StringNameInDoubleQuotes.java
@@ -0,0 +1,9 @@
+public class StringNameInDoubleQuotes {
+    public static void main(String[] args) {
+
+        //\u0053\u006F\u0068\u0061\u0069\u006C -->Sohail
+        String name ="\u0053\u006F\u0068\u0061\u0069\u006C";
+        //Sohail
+        System.out.println(name);
+    }
+}
