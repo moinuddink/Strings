9b9f8e0b36b10a2612fa13e1b741e81fce7d2df0
moinuddin06218 <moinuddin06218@gmail.com>
2025-06-19 22:45:39 -0400

    Commit12

--- src/main/java/SearchingForAValueInString.java
+++ src/main/java/SearchingForAValueInString.java
@@ -3,7 +3,12 @@ public class SearchingForAValueInString {
     public static void main(String[] args) {
 
         String originalString = "Sohail";
+        String falseString = "False";
         boolean isFound = originalString.contains("Sohail");
+        boolean isFound2 = originalString.contains("f");
+        //true
         System.out.println(isFound);
+        //false
+        System.out.println(isFound2);
     }
 }
